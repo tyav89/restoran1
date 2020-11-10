677e05ddb87b6dda5781fcad5b95d7a501475295
Dmitriy Tyavin <tyav89@mail.ru>
2020-11-10 15:30:37 +0300

    Change Restoren.java

--- src/main/java/Restoran.java
+++ src/main/java/Restoran.java
@@ -2,40 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Restoran {
-    final int timeSleep = 2000;
+    final int timeSleep = 1000;
     List<String> listCustomer = new ArrayList<>();
     List<String> listOrder = new ArrayList<>();
+    final Object customer = new Object();
+    final Object waiter = new Object();
 
     public synchronized void makeOrder() {
-        while (listCustomer.size() < 1) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (waiter) {
+            while (listCustomer.size() < 1) {
+                try {
+                    waiter.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            System.out.printf("Официант %s принял заказ у %s\n", Thread.currentThread().getName(), listCustomer.get(0));
+            sleep();
+            System.out.printf("Официант %s несет заказ %s\n", Thread.currentThread().getName(), listCustomer.get(0));
+            listOrder.add(listCustomer.remove(0));
+            orderReady();
         }
-        System.out.printf("Официант %s принял заказ у %s\n", Thread.currentThread().getName(), listCustomer.get(0));
-        sleep();
-        System.out.printf("Официант %s несет заказ %s\n", Thread.currentThread().getName(), listCustomer.get(0));
-        listOrder.add(listCustomer.remove(0));
-        notify();
     }
 
-    public synchronized void eatAndGoOut() {
-        sleep();
-        notify();
-        while (listOrder.size() < 1) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    public void eatAndGoOut() {
+        readyToOrder();
+        synchronized (customer){
+            while (listOrder.size() < 1) {
+                try {
+                    customer.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            listOrder.remove(0);
+            System.out.printf("Посетитель %s ест\n", Thread.currentThread().getName());
+            sleep();
+            System.out.printf("Посетитель %s уходит\n", Thread.currentThread().getName());
+            customer.notify();
         }
-        listOrder.remove(0);
-        System.out.printf("Посетитель %s ест\n", Thread.currentThread().getName());
-        sleep();
-        System.out.printf("Посетитель %s уходит\n", Thread.currentThread().getName());
-        notify();
 
     }
 
@@ -49,5 +54,17 @@ public class Restoran {
 
     public void addCustomer(String name) {
         listCustomer.add(name);
+    }
+
+    private void readyToOrder() {
+        synchronized (waiter) {
+            waiter.notify();
+        }
+    }
+
+    private void orderReady() {
+        synchronized (customer) {
+            customer.notify();
+        }
     }
 }
